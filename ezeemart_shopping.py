17d7cea813ad10eaa33a243f57fc2707b219fb2e
ABDAmaan <abdulamaanmarch27@gmail.com>
2025-05-31 08:15:52 +1000

    Product Management

--- ezeemart_shopping.py
+++ ezeemart_shopping.py
@@ -1,32 +1,53 @@
-customers = {}
 
 # Custmer Registration and Mangement
 
 class Customer:
+  _customers = {}
 # This is a counter to keep count of the client ID and avoid errors
-  next_customer_id = 1
+  _next_customer_id = 1
 
   def __init__(self, customer_id, name):
-    self.customer_id = customer_id
-    self.name = name
+    self._customer_id = customer_id
+    self._name = name
 # This methof return the customer ID
   def get_customer_id(self):
-    return self.customer_id
+    return self._customer_id
   # This method returns the name of the customer
   def get_customer_name(self):
-    return self.name
+    return self._name
   # This method returns the detail or the customer
   def display_customer_detail(self):
-    print(f"Customer Detail\nCustomer ID: {self.customer_id}\nCustomer Name: {self.name}")
+    print(f"Customer Detail\nCustomer ID: {self._customer_id}\nCustomer Name: {self._name}")
 
 #  This uses a class method which allow to modify class level variable and create an instance with throwing an error
   @classmethod
   def register_customer(cls, name):
-    customer_id = cls.next_customer_id
+    customer_id = cls._next_customer_id
     customer = cls(customer_id, name)
-    customers[customer_id] = name
-    cls.next_customer_id += 1 
+    cls._customers[customer_id] = name
+    cls._next_customer_id += 1 
     return customer
 
 
 
+# Product Management
+# encapsulate
+# method for displaying products
+class Product:
+
+  def __init__(self, name, price, availability: bool = True):
+    self._name = name
+    self._price = price
+    if availability == True:
+      self._availability = 'In Stock'
+    else:
+      self._availability = 'Out of Stock'
+
+  def display_product(self):
+    print(f"Product Detail\nName: {self._name}\nPrice: ${self._price} \nAvailability: {self._availability}")
+  
+# sauce = Product('Sauce', '100', True)
+# sauce.display_product()
+
+# Mayo = Product('Sauce', '100', False)
+# Mayo.display_product()
