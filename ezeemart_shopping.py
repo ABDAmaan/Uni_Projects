53aa1a7fea4b35e0c65b89138e1d1e595a7e6149
ABDAmaan <abdulamaanmarch27@gmail.com>
2025-05-31 07:48:32 +1000

    Customer Registration and management

--- ezeemart_shopping.py
+++ ezeemart_shopping.py
@@ -1,30 +1,32 @@
 customers = {}
 
+# Custmer Registration and Mangement
+
 class Customer:
+# This is a counter to keep count of the client ID and avoid errors
+  next_customer_id = 1
+
   def __init__(self, customer_id, name):
     self.customer_id = customer_id
     self.name = name
-
+# This methof return the customer ID
   def get_customer_id(self):
     return self.customer_id
-  
+  # This method returns the name of the customer
   def get_customer_name(self):
     return self.name
-  
+  # This method returns the detail or the customer
   def display_customer_detail(self):
     print(f"Customer Detail\nCustomer ID: {self.customer_id}\nCustomer Name: {self.name}")
 
-  def register_customer(self, name):
-    next_customer_id = 0
-    self.name = name
-    self.customer_id = next_customer_id
-    next_customer_id += 1 
+#  This uses a class method which allow to modify class level variable and create an instance with throwing an error
+  @classmethod
+  def register_customer(cls, name):
+    customer_id = cls.next_customer_id
+    customer = cls(customer_id, name)
+    customers[customer_id] = name
+    cls.next_customer_id += 1 
+    return customer
 
-
-
-client1 = Customer.register_customer(name='amaan')
-client_id = client1.get_customer_id()
-client_name = client1.get_customer_name()
-client1.display_customer_detail()
 
 
